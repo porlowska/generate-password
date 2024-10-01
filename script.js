019e4ea4ea342098b1b15f252772a0c9b3bd240f
Polly Orlowska <polly.orlowska@gmail.com>
2024-10-01 19:10:02 +0200

    fix: add submit button, and button styling

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 import char from "./char";
+let passwordLength = $("#lenght");
+let lowercase = $("#lowercase");
+let uppercase = $("#uppercase");
+let numbers = $("#numbers");
+let special = $("#special");
